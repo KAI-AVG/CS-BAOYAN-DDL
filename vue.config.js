81e0d6db671d28807c6d32cd901a9652daec726b
KAI <69336327+KAI-AVG@users.noreply.github.com>
2025-06-08 12:41:26 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
-  // 其他配置...
-})
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/'
+    : '/'
+};
